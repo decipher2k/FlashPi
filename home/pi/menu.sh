39c57b143a874991024d64ab1db578dd7afe5ca7
decipher <dennis.heine@gmx.de>
2022-12-14 14:02:58 +0100

    Update menu.sh

--- home/pi/menu.sh
+++ home/pi/menu.sh
@@ -1,30 +1,49 @@
 function show(){
 #dialog --title "Select file: (b) Launch: (a)" --fselect `cat /home/pi/choice`/ 20 20 2>/home/pi/choice
 
+let i=0 # define counting variable
+D=() # define working array
+D+=("  .." "")
+while read -r line; do # process file by file
+    let i=$i+1
+    D+=("  $line" "")
+done < <( ls -d -1 */)
 
 let i=0 # define counting variable
 W=() # define working array
 while read -r line; do # process file by file
     let i=$i+1
     W+=("$line" "")
-done < <( ls -1 /home/pi/roms )
-FILE=$(dialog --title "ROM Files" --menu "Choose wisely, young padawan." 0 0 0 "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
+done < <( ls -1 *.gb* )
+
+FILE=$(dialog --title "ROM Files" --menu "Choose wisely, young padawan." 0 0 0 "${D[@]}" "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output
 clear
 
-echo $FILE > /home/pi/choice
+echo "$FILE" > /home/pi/choice
+
+
 
 if [ ! -f /home/pi/shutdown ]; then
 
-if [[ "`cat /home/pi/choice`" == *".gb" ]]; then
-/home/pi/gnuboy-1.0.4/sdlgnuboy --fullscreen=0 `cat /home/pi/choice`
-show
-else
-	echo "/home/pi/roms" > /home/pi/choice
-	show
-fi
-else
-rm /home/pi/shutdown
+    if [[ "`cat /home/pi/choice`" == "  "* ]]; then
 
+        dir="`cat /home/pi/choice`"
+        echo $dir
+        cd $dir
+        show
+    else      
+#        FILE=$(dialog --title "ROM Files" --menu "Choose wisely, young padawan." 0 0 0 "${D[@]}" "${W[@]}" 3>&2 2>&1 1>&3) # show dialog and store output    
+
+        if [[ "`cat /home/pi/choice`" == *".gb"* ]]; then
+            /home/pi/gnuboy-1.0.4/sdlgnuboy --fullscreen=0 `cat /home/pi/choice`
+            show
+        else	             
+                echo "/home/pi/roms" > /home/pi/choice
+	            show
+        fi
+    fi
+else
+        rm /home/pi/shutdown
 fi
 }
 clear
